9b69fa9fd9d9600af233cde80f1624a745a32c16
ssvitlana31 <130747746+ssvitlana31@users.noreply.github.com>
2023-09-19 15:40:51 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const textColor = document.querySelector(".color");
+const changeColorBtnEl = document.querySelector(".change-color");
+const body = document.body;
+
+const changeColor = () => {
+  textColor.textContent = body.style.backgroundColor = getRandomHexColor();
+};
+
+changeColorBtnEl.addEventListener("click", changeColor);
